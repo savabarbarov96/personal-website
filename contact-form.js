09af0c73a97572f2d0efc9cedb1230cb13148598
Sava Barbarov <savabarbarov96@gmail.com>
2022-10-01 15:13:01 +0300

    Update

--- contact-form.js
+++ contact-form.js
@@ -14,3 +14,23 @@ function background(){
   var element = document.getElementById(".container123");
   element.classList.add(".overlay");
 }
+
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach((entry) => {
+
+  console.log(entry)
+  if (entry.isIntersecting){
+    entry.target.classList.add('show');
+  }
+  else {
+    entry.target.classList.remove('show')
+  }
+});
+
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
